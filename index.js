5c05a37466df6c6cb239d41868e763fa48d9603e
ViniH4 <vinigabrielh4@gmail.com>
2024-01-24 17:37:23 -0300

    callbacks
    chamadas para os modulos que fiz

---
+++ index.js
@@ -0,0 +1,9 @@
+const { Person } = require("./person.js");
+
+// require("./modules/path")
+// require("./modules/fs");
+// require("./modules/http");
+
+require("./modules/express.js");
+
+const person = new Person("Vini");
